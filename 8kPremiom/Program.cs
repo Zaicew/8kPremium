eb63f6444edc3c2de0a2974932d675060ea81c57
Daniel Zajas <61469839+Zaicew@users.noreply.github.com>
2021-06-07 12:26:34 +0200

    Add project files.

---
+++ 8kPremiom/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _8kPremiom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task1 = new Task1();
+            Console.WriteLine(task1.BinaryGap(10000));
+            Console.WriteLine(task1.BinaryGap(1222));
+            Console.WriteLine(task1.BinaryGap(12364));
+            Console.WriteLine(task1.BinaryGap(12558));
+            Console.WriteLine(task1.BinaryGap(339988));
+        }
+    }
+}
---
+++ 8kPremiom/Task1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8kPremiom
+{
+    class Task1
+    {
+        public int BinaryGap(int number)
+        {
+            int result = 0;
+            int tmp = 0;
+            string binaryNumber = ConvertoBinary(number);
+
+            foreach (var item in binaryNumber)
+            {
+                if (item != '0') tmp = 0;
+                else
+                {
+                    tmp++;
+                    if (result < tmp) result = tmp;
+                }
+            }
+            return result;
+        }
+
+        private string ConvertoBinary(int number)
+        {
+            string result = "";
+            int tmp;
+            while (number > 0)
+            {
+                tmp = number % 2;
+                number /= 2;
+                result = tmp.ToString() + result;
+            }
+            return result;
+        }
+    }
+}
