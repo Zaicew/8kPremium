8815d1672a3cabf3c42a9d937a155fc1062705d8
Daniel Zajas <61469839+Zaicew@users.noreply.github.com>
2021-06-10 13:59:32 +0200

    task2

--- 8kPremiom/Program.cs
+++ 8kPremiom/Program.cs
@@ -8,11 +8,17 @@ namespace _8kPremiom
         static void Main(string[] args)
         {
             var task1 = new Task1();
+            Console.WriteLine("========================Task1========================");
             Console.WriteLine(task1.BinaryGapRegEx(10000));
             Console.WriteLine(task1.BinaryGapRegEx(1222));
             Console.WriteLine(task1.BinaryGapRegEx(12364));
             Console.WriteLine(task1.BinaryGapRegEx(12558));
             Console.WriteLine(task1.BinaryGapRegEx(339988));
+
+            var task2 = new Task2();
+            Console.WriteLine("========================Task2========================");
+            Console.WriteLine(task2.CountJewelsInStones("z", "ZZ"));
+            Console.WriteLine(task2.CountJewelsInStones("aA", "aAAbbb"));
         }
     }
 }
---
+++ 8kPremiom/Task2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _8kPremiom
+{
+    class Task2
+    {
+        public int CountJewelsInStones(string jewels, string stones)
+        {
+            int result = 0;
+
+            foreach (var item in stones)
+            {
+                foreach (var element in jewels)
+                {
+                    if (element == item) result++;
+                }
+            }
+            return result;
+        }
+
+    }
+}
